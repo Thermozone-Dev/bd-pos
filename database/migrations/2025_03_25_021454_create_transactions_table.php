72968bc1c55b125afd83561bdf737e79b585dded
Angelo Marquez <105302972+FlavensDG@users.noreply.github.com>
2025-03-26 13:31:33 +0800

    Create Transaction Resources

--- database/migrations/2025_03_25_021454_create_transactions_table.php
+++ database/migrations/2025_03_25_021454_create_transactions_table.php
@@ -18,18 +18,18 @@ return new class extends Migration
             $table->string('barcode');
             $table->string('transaction_method');
             $table->float('transaction_fee');
+            $table->float('vatable_sales');
             $table->float('cash_tendered');
             $table->float('change');
-            $table->float('vatable_sales');
             $table->float('vat');
             $table->float('vat_exempt_sales');
             $table->float('vat_exempt');
             $table->float('zero_rated_sales');
-            $table->boolean('is_valid');
-            $table->boolean('is_pwd');
-            $table->boolean('is_sc');
-            $table->boolean('is_nac');
-            $table->boolean('is_soloparent');
+            $table->boolean('is_valid')->nullable();
+            $table->boolean('is_pwd')->nullable();
+            $table->boolean('is_sc')->nullable();
+            $table->boolean('is_nac')->nullable();
+            $table->boolean('is_soloparent')->nullable();
             $table->timestamps();
         });
     }
