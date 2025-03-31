96b85893b514545b37f18a502c1ffed83c46644f
Angelo Marquez <105302972+FlavensDG@users.noreply.github.com>
2025-03-31 11:46:06 +0800

    Adjust minor relationships

--- app/Models/Shift.php
+++ app/Models/Shift.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Shift extends Model
 {
@@ -12,7 +13,7 @@ class Shift extends Model
         'time_out',
     ];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class, 'user_id');
     }
