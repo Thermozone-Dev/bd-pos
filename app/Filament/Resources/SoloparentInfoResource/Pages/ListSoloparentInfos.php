9a6f87dd84c82bcbda1b59b99075987363bc0ef2
Angelo Marquez <105302972+FlavensDG@users.noreply.github.com>
2025-03-31 19:36:08 +0800

    Create BIR E-5 PDF Export

--- app/Filament/Resources/SoloparentInfoResource/Pages/ListSoloparentInfos.php
+++ app/Filament/Resources/SoloparentInfoResource/Pages/ListSoloparentInfos.php
@@ -3,7 +3,9 @@
 namespace App\Filament\Resources\SoloparentInfoResource\Pages;
 
 use App\Filament\Resources\SoloparentInfoResource;
+use Barryvdh\Snappy\Facades\SnappyPdf;
 use Filament\Actions;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\ListRecords;
 
 class ListSoloparentInfos extends ListRecords
@@ -13,7 +15,16 @@ class ListSoloparentInfos extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            //
+            Action::make('downloadPdf')
+                ->icon('heroicon-o-arrow-down-tray')
+                ->label('Download BIR E-5 Report PDF')
+                ->action(function () {
+
+                    $pdf = SnappyPdf::loadView('reports.sp-summary')
+                        ->setPaper('folio', 'landscape');
+
+                    return $pdf->stream('E-5 - Solo Parent Report.pdf');
+                }),
         ];
     }
 }
