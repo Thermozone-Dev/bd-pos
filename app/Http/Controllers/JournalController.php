64ec863c742dad782ecc181982af5f668ce508cd
Eli <eli.quinio17@gmail.com>
2025-04-07 09:52:13 +0800

    Create JournalController.php

---
+++ app/Http/Controllers/JournalController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Database\Eloquent\Casts\Json;
+
+class JournalController extends Controller
+{
+    public function newTransaction($data)
+    {
+        //
+    }
+
+    public function updateTransaction($data, string $id)
+    {
+        //
+    }
+
+    public function createRecord(string $string)
+    {
+        //
+    }
+
+    public function updateRecord(string $search, string $string)
+    {
+        //
+    }
+}
