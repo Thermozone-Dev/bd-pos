5c422310154062aa0555d2a89a350ed190eae740
Eli <eli.quinio17@gmail.com>
2025-04-07 09:55:14 +0800

    Add journal controller to api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\JournalController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// Journal Routes
+Route::get('/journal/create', JournalController::class, 'newTransaction')->name('journal-log-transaction');
+Route::get('/journal/update', JournalController::class, 'updateTransaction')->name('journal-update-transaction');
