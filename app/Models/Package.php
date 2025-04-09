6bbed934c3e6e32def700e0172eaf8f7068597cc
Eli <eli.quinio17@gmail.com>
2025-04-09 19:25:21 +0800

    Add relations for Item, Package, and Product Models

--- app/Models/Package.php
+++ app/Models/Package.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
@@ -16,9 +18,19 @@ class Package extends Model implements HasMedia
         'price',
     ];
 
-    public function products()
+    public function products(): BelongsToMany
     {
         return $this->belongsToMany(Product::class, 'package_has_products', 'package_id', 'product_id')->withPivot('id');
+    }
+
+    public function productsJunction(): HasMany
+    {
+        return $this->hasMany(PackageHasProduct::class, 'package_id', 'id');
+    }
+
+    public function item(): BelongsTo
+    {
+        return $this->belongsTo(Item::class, 'id', 'package_id');
     }
 
 }
