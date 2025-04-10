72edaf0fde7f34051eeed4f9a5e621453a5c2509
Eli <eli.quinio17@gmail.com>
2025-04-10 10:20:33 +0800

    Add api routes for items

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\JournalController;
+use App\Http\Controllers\Api\v1\ItemController;
+use App\Http\Controllers\Api\v1\JournalController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,5 +10,12 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 // Journal Routes
-Route::get('/journal', [JournalController::class, 'newTransaction'])->name('journal-log-transaction');
-Route::get('/journal/{id}', [JournalController::class, 'updateTransaction'])->name('journal-update-transaction');
+Route::get('/v1/journal', [JournalController::class, 'newTransaction']);
+Route::get('/v1/journal/{id}', [JournalController::class, 'updateTransaction']);
+
+// Item Routes
+Route::get('/v1/items', [ItemController::class, 'index']);
+Route::get('/v1/items/{id}', [ItemController::class, 'show']);
+Route::post('/v1/items', [ItemController::class, 'store']);
+Route::put('/v1/items/{id}', [ItemController::class, 'update']);
+Route::delete('/v1/items/{id}', [ItemController::class, 'destroy']);
