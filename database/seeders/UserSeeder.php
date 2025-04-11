4056b9bb40b9f089a1d2bbaed6fa1f787d15ce99
Eli <eli.quinio17@gmail.com>
2025-04-11 10:25:29 +0800

    Update UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Super Admin',
+            'email' => 'super_admin@possikat.com',
+            'password' => Hash::make('pass12345678word'),
+        ]);
+    }
+}
