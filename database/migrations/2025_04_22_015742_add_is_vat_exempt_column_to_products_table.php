322c1c3c900697cab88d11aed8be8a7cf651d429
Eli <eli.quinio17@gmail.com>
2025-04-22 10:39:03 +0800

    Add default values to is vat exempt boolean

--- database/migrations/2025_04_22_015742_add_is_vat_exempt_column_to_products_table.php
+++ database/migrations/2025_04_22_015742_add_is_vat_exempt_column_to_products_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->boolean('is_vat_exempt');
+            $table->boolean('is_vat_exempt')->default(false);
         });
     }
 
--- database/migrations/2025_04_22_020601_add_is_vat_exempt_column_to_packages_table.php
+++ database/migrations/2025_04_22_020601_add_is_vat_exempt_column_to_packages_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('packages', function (Blueprint $table) {
-            $table->boolean('is_vat_exempt');
+            $table->boolean('is_vat_exempt')->default(false);
         });
     }
 
