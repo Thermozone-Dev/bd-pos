b20fe2be8a3c1806062a8c80d1092ff5c95380f7
Eli <eli.quinio17@gmail.com>
2025-04-22 13:29:51 +0800

    Create Payment Method Resource

---
+++ app/Filament/Resources/PaymentMethodResource.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PaymentMethodResource\Pages;
+use App\Models\PaymentMethod;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Columns\Layout\Split;
+use Filament\Tables\Columns\Layout\Stack;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ToggleColumn;
+use Filament\Tables\Table;
+
+class PaymentMethodResource extends Resource
+{
+    protected static ?string $model = PaymentMethod::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-currency-dollar';
+
+    protected static ?string $navigationGroup = 'Items & Discounts';
+
+    protected static ?int $navigationSort = 9;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->columnSpan(1),
+                Section::make()
+                    ->schema([
+                        Toggle::make('is_digital'),
+                        Toggle::make('is_enabled'),
+                    ])
+                    ->columnSpan(1),
+                SpatieMediaLibraryFileUpload::make('logo'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Split::make([
+                    TextColumn::make('name')
+                        ->icon('heroicon-s-banknotes'),
+                    ToggleColumn::make('is_enabled'),
+                ]),
+            ])
+            ->contentGrid([
+                'md' => 2,
+                'xl' => 3,
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                EditAction::make(),
+            ])
+            ->bulkActions([
+                //
+            ])
+            ->recordUrl(null)
+            ->paginated(false);
+
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPaymentMethods::route('/'),
+            'create' => Pages\CreatePaymentMethod::route('/create'),
+            'edit' => Pages\EditPaymentMethod::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Models/PaymentMethod.php
+++ app/Models/PaymentMethod.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
-class PaymentMethod extends Model
+class PaymentMethod extends Model implements HasMedia
 {
     use InteractsWithMedia;
 
