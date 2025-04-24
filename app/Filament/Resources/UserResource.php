414855ffdc9a1f1b0ce6a2ef49ca93bcc020ed18
Eli <eli.quinio17@gmail.com>
2025-04-24 13:48:53 +0800

    Add Role selector in Users Forms

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -63,11 +63,6 @@ class UserResource extends Resource
                             ->placeholder('********')
                             ->visible(fn ($livewire) => $livewire instanceof CreateUser)
                             ->rule(Password::default()),
-                        // Select::make('roles')
-                        //     ->relationship('roles', 'name')
-                        //     ->multiple()
-                        //     ->preload()
-                        //     ->searchable(),
                 ]),
 
                 Section::make('New Password')
@@ -89,6 +84,16 @@ class UserResource extends Resource
                             ->visible(fn ($livewire) => $livewire instanceof EditUser)
                             ->rule(Password::default()),
                 ])->visible(fn ($livewire) => $livewire instanceof EditUser),
+
+                Section::make('Authentication')
+                    ->columns(2)
+                    ->schema([
+                        Select::make('roles')
+                            ->relationship('roles', 'name')
+                            ->multiple()
+                            ->preload()
+                            ->searchable(),
+                    ]),
             ]);
     }
 
