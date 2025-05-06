bc432199b82ff56497d7d77f5fd93b676492ce5a
Angelo Marquez <105302972+FlavensDG@users.noreply.github.com>
2025-05-06 22:48:23 +0800

    Integrate NAC Report Backend Partially

--- app/Filament/Resources/NacInfoResource/Pages/ListNacInfos.php
+++ app/Filament/Resources/NacInfoResource/Pages/ListNacInfos.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\NacInfoResource\Pages;
 
 use App\Filament\Resources\NacInfoResource;
+use App\Models\NacInfo;
+use App\Models\Transaction;
 use Barryvdh\Snappy\Facades\SnappyPdf;
 use Filament\Actions\Action;
+use Filament\Forms\Components\DatePicker;
 use Filament\Resources\Pages\ListRecords;
 
 class ListNacInfos extends ListRecords
@@ -17,10 +20,29 @@ class ListNacInfos extends ListRecords
             Action::make('downloadPdf')
                 ->icon('heroicon-o-arrow-down-tray')
                 ->label('Download BIR E-4 Report PDF')
-                ->action(function () {
+                ->form([
+                    DatePicker::make('start_date')
+                        ->label('Start Date')
+                        ->required()
+                        ->default(now()->endOfDay()),
+                    DatePicker::make('end_date')
+                        ->label('End Date')
+                        ->required()
+                        ->default(now()->endOfWeek()),
+                ])
+                ->action(function (array $data) {
 
-                    $pdf = SnappyPdf::loadView('reports.nac-summary')
-                        ->setPaper('folio', 'landscape');
+                    $nacInfos = NacInfo::query()
+                        ->whereBetween('created_at', [$data['start_date'], $data['end_date']])
+                        ->get();
+
+                    $transactionIds = $nacInfos->pluck('transaction_id')->unique();
+                    $nacTransactions = Transaction::whereIn('id', $transactionIds)->get()->keyBy('id');
+
+                    $pdf = SnappyPdf::loadView('reports.nac-summary', [
+                        'nacInfos' => $nacInfos,
+                        'nacTransactions' => $nacTransactions,
+                    ])->setPaper('folio', 'landscape');
 
                     return $pdf->stream('E-4 - National Athletes and Coaches Report.pdf');
                 }),
