9517ddd1987677fa74478921f3594570d4c5de67
Angelo Marquez <105302972+FlavensDG@users.noreply.github.com>
2025-05-06 20:06:29 +0800

    Integrate SC Report to Backend
    - Partial BIR E1 Report Integration

--- app/Filament/Resources/ScInfoResource/Pages/ListScInfos.php
+++ app/Filament/Resources/ScInfoResource/Pages/ListScInfos.php
@@ -3,9 +3,12 @@
 namespace App\Filament\Resources\ScInfoResource\Pages;
 
 use App\Filament\Resources\ScInfoResource;
+use App\Models\ScInfo;
+use App\Models\Transaction;
 use Barryvdh\Snappy\Facades\SnappyPdf;
 use Filament\Actions;
 use Filament\Actions\Action;
+use Filament\Forms\Components\DatePicker;
 use Filament\Resources\Pages\ListRecords;
 
 class ListScInfos extends ListRecords
@@ -18,10 +21,29 @@ class ListScInfos extends ListRecords
             Action::make('downloadPdf')
                 ->icon('heroicon-o-arrow-down-tray')
                 ->label('Download BIR E-2 Report PDF')
-                ->action(function () {
+                ->form([
+                    DatePicker::make('start_date')
+                        ->label('Start Date')
+                        ->required()
+                        ->default(now()->endOfDay()),
+                    DatePicker::make('end_date')
+                        ->label('End Date')
+                        ->required()
+                        ->default(now()->endOfWeek()),
+                ])
+                ->action(function (array $data) {
 
-                    $pdf = SnappyPdf::loadView('reports.sc-summary')
-                        ->setPaper('folio', 'landscape');
+                    $scInfos = ScInfo::query()
+                        ->whereBetween('created_at', [$data['start_date'], $data['end_date']])
+                        ->get();
+
+                    $transactionIds = $scInfos->pluck('transaction_id')->unique();
+                    $scTransactions = Transaction::whereIn('id', $transactionIds)->get()->keyBy('id');
+
+                    $pdf = SnappyPdf::loadView('reports.sc-summary', [
+                        'scInfos' => $scInfos,
+                        'scTransactions' => $scTransactions,
+                    ])->setPaper('folio', 'landscape');
 
                     return $pdf->stream('E-2 - Senior Citizen Report.pdf');
                 }),
