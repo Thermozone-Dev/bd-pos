c7ba589d240bd6ee36932799c835ba3c2257cbd0
Angelo Marquez <105302972+FlavensDG@users.noreply.github.com>
2025-05-06 22:12:28 +0800

    Integrate PWD and Partial Solo Parent Report Backend
    - Discount Field for Solo Parent Pending

--- app/Filament/Resources/PwdInfoResource/Pages/ListPwdInfos.php
+++ app/Filament/Resources/PwdInfoResource/Pages/ListPwdInfos.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\PwdInfoResource\Pages;
 
 use App\Filament\Resources\PwdInfoResource;
+use App\Models\PwdInfo;
+use App\Models\Transaction;
 use Barryvdh\Snappy\Facades\SnappyPdf;
 use Filament\Actions\Action;
+use Filament\Forms\Components\DatePicker;
 use Filament\Resources\Pages\ListRecords;
 
 class ListPwdInfos extends ListRecords
@@ -17,10 +20,29 @@ class ListPwdInfos extends ListRecords
             Action::make('downloadPdf')
                 ->icon('heroicon-o-arrow-down-tray')
                 ->label('Download BIR E-3 Report PDF')
-                ->action(function () {
+                ->form([
+                    DatePicker::make('start_date')
+                        ->label('Start Date')
+                        ->required()
+                        ->default(now()->endOfDay()),
+                    DatePicker::make('end_date')
+                        ->label('End Date')
+                        ->required()
+                        ->default(now()->endOfWeek()),
+                ])
+                ->action(function (array $data) {
 
-                    $pdf = SnappyPdf::loadView('reports.pwd-summary')
-                        ->setPaper('folio', 'landscape');
+                    $pwdInfos = PwdInfo::query()
+                        ->whereBetween('created_at', [$data['start_date'], $data['end_date']])
+                        ->get();
+
+                    $transactionIds = $pwdInfos->pluck('transaction_id')->unique();
+                    $pwdTransactions = Transaction::whereIn('id', $transactionIds)->get()->keyBy('id');
+
+                    $pdf = SnappyPdf::loadView('reports.pwd-summary', [
+                        'pwdInfos' => $pwdInfos,
+                        'pwdTransactions' => $pwdTransactions,
+                    ])->setPaper('folio', 'landscape');
 
                     return $pdf->stream('E-3 - Persons with Disabilities Report.pdf');
                 }),
