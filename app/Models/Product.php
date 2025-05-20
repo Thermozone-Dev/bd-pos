ca549a9f05dce4458619ab8330c33076228a92a4
Eli <eli.quinio17@gmail.com>
2025-05-20 11:47:14 +0800

    remove Model logging in products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,14 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
-use Spatie\Activitylog\LogOptions;
-use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
 class Product extends Model implements HasMedia
 {
-    use InteractsWithMedia, LogsActivity;
+    use InteractsWithMedia;
 
     protected $fillable = [
         'name',
@@ -21,13 +19,6 @@ class Product extends Model implements HasMedia
         'product_type_id',
         'sku',
     ];
-
-    public function getActivitylogOptions(): LogOptions
-    {
-        return LogOptions::defaults()
-        ->logOnly(['name', 'price', 'product_type_id', 'sku']);
-        // Chain fluent methods for configuration options
-    }
 
     public function packages(): BelongsToMany
     {
