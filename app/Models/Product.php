2c49d01c2b5ab98a38f1407f3e187699d4abfccd
Eli <eli.quinio17@gmail.com>
2025-05-20 11:31:44 +0800

    Integrate product type relation to product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,12 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
 class Product extends Model implements HasMedia
 {
-    use InteractsWithMedia;
+    use InteractsWithMedia, LogsActivity;
 
     protected $fillable = [
         'name',
@@ -19,9 +22,21 @@ class Product extends Model implements HasMedia
         'sku',
     ];
 
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+        ->logOnly(['name', 'price', 'product_type_id', 'sku']);
+        // Chain fluent methods for configuration options
+    }
+
     public function packages(): BelongsToMany
     {
         return $this->belongsToMany(Package::class, 'package_has_products', 'product_id', 'package_id');
+    }
+
+    public function productType(): HasOne
+    {
+        return $this->hasOne(ProductType::class, 'id', 'product_type_id');
     }
 
     public function item(): BelongsTo
