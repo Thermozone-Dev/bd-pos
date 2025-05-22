fb29dd6485fa5ba1b1f19045cf61326f4451eecf
Eli <eli.quinio17@gmail.com>
2025-05-22 21:41:33 +0800

    Implement Login in Payment Methods

---
+++ app/Filament/Loggers/PaymentMethodLogger.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Filament\Loggers;
+
+use App\Enums\isEnabled;
+use App\Filament\Resources\PaymentMethodResource;
+use App\Models\PaymentMethod;
+use Illuminate\Contracts\Support\Htmlable;
+use Noxo\FilamentActivityLog\Loggers\Logger;
+use Noxo\FilamentActivityLog\ResourceLogger\Field;
+use Noxo\FilamentActivityLog\ResourceLogger\RelationManager;
+use Noxo\FilamentActivityLog\ResourceLogger\ResourceLogger;
+
+class PaymentMethodLogger extends Logger
+{
+    public static ?string $model = PaymentMethod::class;
+
+    public static function getLabel(): string | Htmlable | null
+    {
+        return PaymentMethodResource::getModelLabel();
+    }
+
+    public static function resource(ResourceLogger $logger): ResourceLogger
+    {
+        return $logger
+            ->fields([
+                Field::make('name')
+                    ->label('Name'),
+                Field::make('is_enabled')
+                    ->label('Enabled'),
+                Field::make('is_digital')
+                    ->label('Digital'),
+                Field::make('logo')
+                    ->media(gallery:true),
+            ])
+            ->relationManagers([
+                //
+            ]);
+    }
+}
