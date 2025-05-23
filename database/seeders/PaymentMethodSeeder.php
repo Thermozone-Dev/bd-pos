edc49726f14fecaafcb419037fac1b7014d09600
thirdy0990 <thirdymaturan16@gmail.com>
2025-05-23 08:52:38 +0800

    UpdatePaymentMethodSeeder

--- database/seeders/PaymentMethodSeeder.php
+++ database/seeders/PaymentMethodSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\PaymentMethod;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,35 @@ class PaymentMethodSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        PaymentMethod::factory()->create([
+            'name' => 'Cash',
+            'is_digital' => false,
+            'is_enabled' => true,
+        ]);
+
+        PaymentMethod::factory()->create([
+            'name' => 'Gcash',
+            'is_digital' => true,
+            'is_enabled' => true,
+        ]);
+
+        PaymentMethod::factory()->create([
+            'name' => 'Maya',
+            'is_digital' => true,
+            'is_enabled' => true,
+        ]);
+
+        PaymentMethod::factory()->create([
+            'name' => 'Debit Card',
+            'is_digital' => true,
+            'is_enabled' => true,
+        ]);
+
+        PaymentMethod::factory()->create([
+            'name' => 'Credit Card',
+            'is_digital' => true,
+            'is_enabled' => true,
+        ]);
+
     }
 }
