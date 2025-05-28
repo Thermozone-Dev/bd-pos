30d2f25e4066e87187bc289359f6ac93b3ce73cc
Eli <eli.quinio17@gmail.com>
2025-05-28 13:57:17 +0800

    Fix transaction discount processing

--- app/Http/Controllers/Api/v1/TransactionController.php
+++ app/Http/Controllers/Api/v1/TransactionController.php
@@ -99,21 +99,17 @@ class TransactionController extends Controller
             $_gov_discount_list = $_basket_item_data[1];
 
             // Per Basket Discount
-            if (!isNull($request['transaction_discounts'])) {
-                foreach ($request['transaction_discounts'] as $data) {
-                    $_discount = Discount::find($data['id']);
+            if (!(count($request->transaction_discounts) === 0)) {
+                $_discount = Discount::find($request->transaction_discounts['id']);
 
-                    return
+                $_basket_has_discount_data = [
+                    'transaction_basket_id' => $_basket->id,
+                    'discount_id' => $_discount->id,
+                ];
+                $_basket_has_discount = TransactionBasketHasDiscount::create($_basket_has_discount_data);
 
-                    $_basket_has_discount_data = [
-                        'basket_basket_id' => $_basket->id,
-                        'discount_id' => $_discount->id,
-                    ];
-                    $_basket_has_discount = TransactionBasketHasDiscount::create($_basket_has_discount_data);
-
-                    //Add Gov Discount Processing
-                    $_gov_discount_list = $this->getDiscounts($data, $_gov_discount_list);
-                }
+                //Add Gov Discount Processing
+                $_gov_discount_list = $this->getDiscounts($request->transaction_discounts, $_gov_discount_list);
             }
 
 
@@ -265,10 +261,8 @@ class TransactionController extends Controller
             $_basket_item = TransactionBasketItem::create($_item_data);
 
             // Discount Check
-
-
             // Per Item Discounts
-            if (isset($item['item_discounts'])) {
+            if (!(count($item['item_discounts']) === 0)) {
                 $data = $item['item_discounts']['id'];
 
                 $_discount = Discount::find($data);
