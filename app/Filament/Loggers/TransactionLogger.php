78cbfb4f12a6d8b22a49874f3e5f99f0c81b5459
Eli <eli.quinio17@gmail.com>
2025-05-28 11:24:53 +0800

    Add Public Files

--- app/Filament/Loggers/TransactionLogger.php
+++ app/Filament/Loggers/TransactionLogger.php
@@ -23,47 +23,18 @@ class TransactionLogger extends Logger
     {
         return $logger
             ->fields([
-                // Field::make('processed_by')
-                //     ->label('Processed By'),
                 Field::make('transaction_basket_id')
                     ->label('Transaction Basket ID'),
-                // Field::make('barcode')
-                //     ->label('Barcode'),
                 Field::make('transaction_method_id')
                     ->label('Transaction Method ID'),
-                // Field::make('transaction_fee')
-                //     ->label('Transaction Fee'),
-                // Field::make('cash_tendered')
-                //     ->label('Cash Tendered'),
-                // Field::make('change')
-                //     ->label('Change'),
-                // Field::make('gross_sales')
-                //     ->label('Gross Sales'),
-                // Field::make('vatable_sales')
-                //     ->label('VATable Sales'),
-                // Field::make('vat')
-                //     ->label('VAT'),
-                // Field::make('vat_exempt_sales')
-                //     ->label('VAT Exempt Sales'),
-                // Field::make('vat_exempt')
-                //     ->label('VAT Exempt'),
-                // Field::make('zero_rated_sales')
-                //     ->label('Zero Rated Sales'),
                 Field::make('total_sales')
                     ->label('Total Sales'),
                 Field::make('is_valid')
                     ->label('Is Valid'),
-                // Field::make('is_pwd')
-                //     ->label('Is PWD'),
-                // Field::make('is_sc')
-                //     ->label('Is SC'),
-                // Field::make('is_nac')
-                //     ->label('Is NAC'),
-                // Field::make('is_soloparent')
-                //     ->label('Is Solo Parent'),
             ])
             ->relationManagers([
                 //
             ]);
     }
+
 }
