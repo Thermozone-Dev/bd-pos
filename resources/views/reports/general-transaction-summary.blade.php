de84a37b5f8dd0fcae769ce7e48f95f3c88da5c5
Angelo Marquez <105302972+FlavensDG@users.noreply.github.com>
2025-06-04 02:56:59 +0800

    Update general-transaction-summary.blade.php

--- resources/views/reports/general-transaction-summary.blade.php
+++ resources/views/reports/general-transaction-summary.blade.php
@@ -10,61 +10,53 @@
         <p>Machine Identification Number: <b>XXXXXXXXXX</b></p>
         <p>POS Terminal Number: <b>XXX</b></p>
 
-        <p>Date & Time Generated: <b>XXXXXXXXX</b></p>
+        <p>Date & Time Generated: <b>
+            @php
+                echo \Carbon\Carbon::now()->format('F j, Y h:i A');
+            @endphp</b>
+        </p>
         <p>Issued by: <b>{{ Auth::user()->name }}</b></p>
     </div><br>
     <table style="width: 100%; border-collapse: collapse; font-size: 14px;">
         <caption style="border: 1px solid black; width: 100%; padding: 1em 0; background-color: white; color: black;"><b>General Transaction Summary Report</b></caption>
         <thead>
             <tr>
-                <th rowspan="2" style="background-color: #a6a6a6; border: 1px solid black; padding: 8px; text-align: center;">Transaction ID</th>
-                <th rowspan="2" style="background-color: #a6a6a6; border: 1px solid black; padding: 8px; text-align: center;">Processed By</th>
-                <th rowspan="2" style="background-color: #a6a6a6; border: 1px solid black; padding: 8px; text-align: center;">Created At</th>
-
-
-                <th colspan="6" style="background-color: #ffc000; border: 1px solid black; padding: 8px; text-align: center;">Values</th>
-
-                <th rowspan="2" style="background-color: #a6a6a6; border: 1px solid black; padding: 8px; text-align: center;">Zero Rated Sales</th>
-
-                <th colspan="4" style="background-color: #92d050; border: 1px solid black; padding: 8px; text-align: center;">Discounts</th>
-
-                <th rowspan="2" style="background-color: #a6a6a6; border: 1px solid black; padding: 8px; text-align: center;">Total Sales</th>
-                <th rowspan="2" style="background-color: #a6a6a6; border: 1px solid black; padding: 8px; text-align: center;">Gross Sales</th>
-            </tr>
-            <tr>
+                <th style="background-color: #a6a6a6; border: 1px solid black; padding: 8px; text-align: center;">Transaction ID</th>
+                <th style="background-color: #a6a6a6; border: 1px solid black; padding: 8px; text-align: center;">Processed By</th>
+                <th style="background-color: #a6a6a6; border: 1px solid black; padding: 8px; text-align: center;">Date</th>
                 <th style="background-color: #ffc000; border: 1px solid black; padding: 8px; text-align: center;">Cash Tendered</th>
                 <th style="background-color: #ffc000; border: 1px solid black; padding: 8px; text-align: center;">Transaction Fee</th>
                 <th style="background-color: #ffc000; border: 1px solid black; padding: 8px; text-align: center;">Vatable Sales</th>
-                <th style="background-color: #ffc000; border: 1px solid black; padding: 8px; text-align: center;">Vat</th>
-                <th style="background-color: #ffc000; border: 1px solid black; padding: 8px; text-align: center;">Vat Exempt Sales</th>
+                <th style="background-color: #ffc000; border: 1px solid black; padding: 8px; text-align: center;">VAT</th>
+                <th style="background-color: #ffc000; border: 1px solid black; padding: 8px; text-align: center;">VAT Exempt Sales</th>
                 <th style="background-color: #ffc000; border: 1px solid black; padding: 8px; text-align: center;">Change</th>
-
-
+                <th style="background-color: #a6a6a6; border: 1px solid black; padding: 8px; text-align: center;">Zero Rated Sales</th>
                 <th style="background-color: #92d050; border: 1px solid black; padding: 8px; text-align: center;">PWD</th>
                 <th style="background-color: #92d050; border: 1px solid black; padding: 8px; text-align: center;">Senior Citizen</th>
                 <th style="background-color: #92d050; border: 1px solid black; padding: 8px; text-align: center;">National Athletes and Coaches</th>
                 <th style="background-color: #92d050; border: 1px solid black; padding: 8px; text-align: center;">Solo Parent</th>
+                <th style="background-color: #a6a6a6; border: 1px solid black; padding: 8px; text-align: center;">Total Sales</th>
+                <th style="background-color: #a6a6a6; border: 1px solid black; padding: 8px; text-align: center;">Gross Sales</th>
             </tr>
         </thead>
         @foreach ($transactions as $transaction)
-
             <tr style="background-color: white;">
-                <td style="border: 1px solid black; padding: 8px;">Test{{ $transaction->id }}</td>
-                <td style="border: 1px solid black; padding: 8px;">Test{{ $transaction->processed_by }}</td>
-                <td style="border: 1px solid black; padding: 8px;">Test{{ $transaction->created_at }}</td>
-                <td style="border: 1px solid black; padding: 8px;">Test{{ $transaction->zero_rated_sales }}</td>
-                <td style="border: 1px solid black; padding: 8px;">Test{{ $transaction->total_sales }}</td>
-                <td style="border: 1px solid black; padding: 8px;">Test{{ $transaction->gross_sales }}</td>
-                <td style="border: 1px solid black; padding: 8px;">Test{{ $transaction->cash_tendered }}</td>
-                <td style="border: 1px solid black; padding: 8px;">Test{{ $transaction->transaction_fee }}</td>
-                <td style="border: 1px solid black; padding: 8px;">Test{{ $transaction->vatable_sales }}</td>
-                <td style="border: 1px solid black; padding: 8px;">Test{{ $transaction->vat }}</td>
-                <td style="border: 1px solid black; padding: 8px;">Test{{ $transaction->vat_exempt_sales }}</td>
-                <td style="border: 1px solid black; padding: 8px;">Test{{ $transaction->change }}</td>
-                <td style="border: 1px solid black; padding: 8px;">Test{{ $transaction->is_pwd }}</td>
-                <td style="border: 1px solid black; padding: 8px;">Test{{ $transaction->is_sc }}</td>
-                <td style="border: 1px solid black; padding: 8px;">Test{{ $transaction->is_nac }}</td>
-                <td style="border: 1px solid black; padding: 8px;">Test{{ $transaction->is_soloparent }}</td>
+                <td style="border: 1px solid black; padding: 8px;">{{ str_pad($transaction->id, 6, '0', STR_PAD_LEFT) }}</td>
+                <td style="border: 1px solid black; padding: 8px;">{{ $transaction->processed_by }}</td>
+                <td style="border: 1px solid black; padding: 8px;">{{ date_format($transaction->created_at, 'M d, Y') }}</td>
+                <td style="border: 1px solid black; padding: 8px;">{{ number_format($transaction->cash_tendered, 2) }}</td>
+                <td style="border: 1px solid black; padding: 8px;">{{ number_format($transaction->transaction_fee, 2) }}</td>
+                <td style="border: 1px solid black; padding: 8px;">{{ number_format($transaction->vatable_sales, 2) }}</td>
+                <td style="border: 1px solid black; padding: 8px;">{{ number_format($transaction->vat, 2) }}</td>
+                <td style="border: 1px solid black; padding: 8px;">{{ number_format($transaction->vat_exempt_sales, 2) }}</td>
+                <td style="border: 1px solid black; padding: 8px;">{{ number_format($transaction->change, 2) }}</td>
+                <td style="border: 1px solid black; padding: 8px;">{{ number_format($transaction->zero_rated_sales, 2) }}</td>
+                <td style="border: 1px solid black; padding: 8px;">{{ $transaction->is_pwd }}</td>
+                <td style="border: 1px solid black; padding: 8px;">{{ $transaction->is_sc }}</td>
+                <td style="border: 1px solid black; padding: 8px;">{{ $transaction->is_nac }}</td>
+                <td style="border: 1px solid black; padding: 8px;">{{ $transaction->is_soloparent }}</td>
+                <td style="border: 1px solid black; padding: 8px;">{{ number_format($transaction->total_sales, 2) }}</td>
+                <td style="border: 1px solid black; padding: 8px;">{{ number_format($transaction->gross_sales, 2) }}</td>
             </tr>
         @endforeach
     </table>
