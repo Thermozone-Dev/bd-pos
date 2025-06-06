cdc417ca9c701d83d665ebdf0523a90e8307a4b4
Dennis Enraca <dennisenraca25@gmail.com>
2025-06-06 14:16:25 +0800

    Add API endpoint for role base daily summary

---
+++ database/migrations/2025_06_05_114720_add_cashier_role.php
@@ -0,0 +1,42 @@
+<?php
+
+use BezhanSalleh\FilamentShield\Support\Utils;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Spatie\Permission\PermissionRegistrar;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::table('roles')
+            ->insertGetId([
+                'name' => 'Cashier',
+                'guard_name' => 'web',
+                'created_at' => \Carbon\Carbon::now(),
+                'updated_at' => \Carbon\Carbon::now(),
+            ]);
+
+        $volunteer = Utils::getRoleModel()::where('name', 'Cashier')->first();
+
+        $permissions = Utils::getPermissionModel()::get(); //assigning all existing permission
+
+        $volunteer->givePermissionTo($permissions);
+
+        app(PermissionRegistrar::class)->forgetCachedPermissions();
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
---
+++ database/seeders/CashierAndManagerSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use BezhanSalleh\FilamentShield\Support\Utils;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class CashierAndManagerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        $cashier = User::factory()->create([
+            'name' => 'Cashiers Account',
+            'email' => 'cashier@possikat.com',
+            'password' => Hash::make('password123'),
+        ]);
+        $role = Utils::getRoleModel()::where('name', 'Cashier')->first();
+
+        DB::table('model_has_roles')->insert([
+            'role_id' => $role->id,
+            'model_type' => 'App\Models\User',
+            'model_id' => $cashier->id,
+        ]);
+
+        $manager = User::factory()->create([
+            'name' => 'Managers Account',
+            'email' => 'manager@possikat.com',
+            'password' => Hash::make('password123'),
+        ]);
+
+        $role = Utils::getRoleModel()::where('name', 'Manager')->first();
+
+        DB::table('model_has_roles')->insert([
+            'role_id' => $role->id,
+            'model_type' => 'App\Models\User',
+            'model_id' => $manager->id,
+        ]);
+
+    }
+}
