aac26a80fceb9fb86b0aa0002ac0467e44885f59
Eli <eli.quinio17@gmail.com>
2025-07-18 16:43:10 +0800

    Cast product tax categry enum in product and package models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\ProductTaxCategory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -15,11 +16,15 @@ class Product extends Model implements HasMedia
 {
     use InteractsWithMedia, LogsActivity;
 
+    protected $casts = [
+        'product_tax_category' => ProductTaxCategory::class,
+    ];
+
     protected $fillable = [
         'name',
         'price',
         'product_type_id',
-        'product-tax-category',
+        'product_tax_category',
         'pax',
         'sku',
     ];
