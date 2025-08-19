9aaa33a8b784630575d736926e712b1ecca2903b
Eli <eli.quinio17@gmail.com>
2025-08-19 22:39:08 +0800

    Update ListTransactions.php

--- app/Filament/Resources/TransactionResource/Pages/ListTransactions.php
+++ app/Filament/Resources/TransactionResource/Pages/ListTransactions.php
@@ -31,7 +31,7 @@ class ListTransactions extends ListRecords
                 ->action(
                     function () {
                         if (Storage::disk('public')->exists(Journal::getJournalPath())){
-                            Storage::disk('public')->download(Journal::getJournalPath(), 'eJournal.txt');
+                            Storage::download(Storage::disk('public')->path(Journal::getJournalPath()), 'eJournal.txt');
                         }
                         else {
                             dd('NO FILE');
