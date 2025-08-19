8b198dd834e01173bf7ed02e1847e75449ddb6e1
Eli <eli.quinio17@gmail.com>
2025-08-19 22:42:46 +0800

    Fix file path for journal public

--- app/Filament/Resources/TransactionResource/Pages/ListTransactions.php
+++ app/Filament/Resources/TransactionResource/Pages/ListTransactions.php
@@ -31,7 +31,7 @@ class ListTransactions extends ListRecords
                 ->action(
                     function () {
                         if (Storage::disk('public')->exists(Journal::getJournalPath())){
-                            Storage::download(asset(Journal::getJournalPath()), 'eJournal.txt');
+                            Storage::download('storage/'.Journal::getJournalPath(), 'eJournal.txt');
                         }
                         else {
                             dd('NO FILE');
