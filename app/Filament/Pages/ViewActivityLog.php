77c175a5d0c0e5aabb4be06035f40086db709793
Eli <eli.quinio17@gmail.com>
2025-08-19 18:10:49 +0800

    Update ViewActivityLog.php

--- app/Filament/Pages/ViewActivityLog.php
+++ app/Filament/Pages/ViewActivityLog.php
@@ -27,7 +27,68 @@ class ViewActivityLog extends ListActivities
                 ->icon('heroicon-o-arrow-down-tray')
                 ->color('primary')
                 ->action(function () {
-                    $this->exportLogs();
+                    $filters = $this->getFilters();
+
+                    // Export To PDF using Snappy
+                    $activities = Activity::query();
+
+                    foreach ($filters as $column => $filter)
+                    {
+                        if ($column === 'date_range') {
+                            if ( $filter == null ) {
+                                continue;
+                            }
+                            $dates = explode(' - ', $filter);
+                            $startDate = Carbon::createFromFormat('d/m/Y', $dates[0])->startOfDay();
+                            $endDate = Carbon::createFromFormat('d/m/Y', $dates[1])->endOfDay();
+                            $activities = $activities->whereBetween('created_at', [$startDate, $endDate]);
+                        } elseif ($column === 'causer') {
+                            if ( $filter == null ) {
+                                continue;
+                            }
+                            $id = $filter[-1];
+                            $activities = $activities->whereHas('causer', function ($q) use ($id) {
+                                $q->where('name', 'like', "%{$id}%");
+                            });
+                        } elseif ($column === 'subject_type') {
+                            if ( $filter == null ) {
+                                continue;
+                            }
+                            $activities = $activities->where('subject_type', $filter);
+                        } elseif ($column === 'subject_id') {
+                            if ( $filter == null ) {
+                                continue;
+                            }
+                            $activities = $activities->where('subject_id', $filter);
+                        } elseif ($column === 'event') {
+                            if ( $filter == null ) {
+                                continue;
+                            }
+                            $activities = $activities->where('event', $filter);
+                        }
+                    }
+
+                    $activities = $activities->orderByDesc('created_at')->get();
+
+                    $exportData = [];
+                    foreach($activities as $activity){
+                        array_push($exportData, [
+                            'id' => $activity->id ?? 'N/A',
+                            'event' => $activity->event ?? 'N/A',
+                            'causer_id' => $activity->causer_id ?? 'N/A',
+                            'causer' => $activity->causer_id == null ? 'N/A' : User::find($activity->causer_id)->name,
+                            'subject_type' => class_basename($activity->subject_type) ?? 'N/A',
+                            'subject_id' => $activity->subject_id ?? 'N/A',
+                            'properties' => json_encode($activity->properties) ?? 'N/A',
+                            'created_at' => $activity->created_at ?? 'N/A',
+                        ]);
+                    };
+
+                    $pdf = SnappyPdf::loadView('reports.activity-log', [
+                        'activities' => $exportData,
+                    ])->setPaper('folio', 'landscape');
+
+                    return $pdf->stream('activity_log.pdf');
                 }),
         ];
     }
@@ -39,71 +100,5 @@ class ViewActivityLog extends ListActivities
         );
 
         return count($values) === 0;
-    }
-
-    public function exportLogs()
-    {
-        $filters = $this->getFilters();
-
-        // Export To PDF using Snappy
-        $activities = Activity::query();
-
-        foreach ($filters as $column => $filter)
-        {
-            if ($column === 'date_range') {
-                if ( $filter == null ) {
-                    continue;
-                }
-                $dates = explode(' - ', $filter);
-                $startDate = Carbon::createFromFormat('d/m/Y', $dates[0])->startOfDay();
-                $endDate = Carbon::createFromFormat('d/m/Y', $dates[1])->endOfDay();
-                $activities = $activities->whereBetween('created_at', [$startDate, $endDate]);
-            } elseif ($column === 'causer') {
-                if ( $filter == null ) {
-                    continue;
-                }
-                $id = $filter[-1];
-                $activities = $activities->whereHas('causer', function ($q) use ($id) {
-                    $q->where('name', 'like', "%{$id}%");
-                });
-            } elseif ($column === 'subject_type') {
-                if ( $filter == null ) {
-                    continue;
-                }
-                $activities = $activities->where('subject_type', $filter);
-            } elseif ($column === 'subject_id') {
-                if ( $filter == null ) {
-                    continue;
-                }
-                $activities = $activities->where('subject_id', $filter);
-            } elseif ($column === 'event') {
-                if ( $filter == null ) {
-                    continue;
-                }
-                $activities = $activities->where('event', $filter);
-            }
-        }
-
-        $activities = $activities->orderByDesc('created_at')->get();
-
-        $exportData = [];
-        foreach($activities as $activity){
-            array_push($exportData, [
-                'id' => $activity->id ?? 'N/A',
-                'event' => $activity->event ?? 'N/A',
-                'causer_id' => $activity->causer_id ?? 'N/A',
-                'causer' => $activity->causer_id == null ? 'N/A' : User::find($activity->causer_id)->name,
-                'subject_type' => class_basename($activity->subject_type) ?? 'N/A',
-                'subject_id' => $activity->subject_id ?? 'N/A',
-                'properties' => json_encode($activity->properties) ?? 'N/A',
-                'created_at' => $activity->created_at ?? 'N/A',
-            ]);
-        };
-
-        $pdf = SnappyPdf::loadView('reports.activity-log', [
-            'activities' => $exportData,
-        ])->setPaper('folio', 'landscape');
-
-        return $pdf->stream('activity_log.pdf');
     }
 }
