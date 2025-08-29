cfde993e37bd09b96fa2d7996e056344dab69401
Eli <eli.quinio17@gmail.com>
2025-08-29 14:31:25 +0800

    Fix SC VAT Exempt Sales data call

--- resources/views/reports/report_table/sc.blade.php
+++ resources/views/reports/report_table/sc.blade.php
@@ -30,7 +30,7 @@
             <td colspan="4" style="border: 1px solid black; padding: 8px;">{{ str_pad($scInfo->transaction_id, 12, '0', STR_PAD_LEFT) }}</td>
             <td colspan="4" style="border: 1px solid black; padding: 8px;">{{  number_format($scTransaction->gross_sales, 2) }}</td>
             <td colspan="4" style="border: 1px solid black; padding: 8px;">{{  number_format($scTransaction->vat, 2) }}</td>
-            <td colspan="4" style="border: 1px solid black; padding: 8px;">{{  number_format($scTransaction->vat_exempt, 2) }}</td>
+            <td colspan="4" style="border: 1px solid black; padding: 8px;">{{  number_format($scTransaction->vat_exempt_sales, 2) }}</td>
             <td colspan="4" style="border: 1px solid black; padding: 8px; text-align: center;">0</td>
             <td colspan="4" style="border: 1px solid black; padding: 8px; text-align: center;">&#x2713;</td>
             <td colspan="4" style="border: 1px solid black; padding: 8px;">{{  number_format($scTransaction->total_sales, 2) }}</td>
