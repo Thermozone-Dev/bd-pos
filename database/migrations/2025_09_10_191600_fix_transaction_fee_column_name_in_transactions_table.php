04f94ce3a8381853966a528271151f6829ac34bb
Eli <eli.quinio17@gmail.com>
2025-09-10 20:05:34 +0800

    Hotfix transaction fee

---
+++ database/migrations/2025_09_10_191600_fix_transaction_fee_column_name_in_transactions_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->renameColumn('transaction_fee', 'total_transaction_fee');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->renameColumn('total_transaction_fee', 'transaction_fee');
+        });
+    }
+};
