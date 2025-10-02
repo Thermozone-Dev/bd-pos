60e8ccccfbb98d9f2e7cab1b6455864d32f67463
Eli <eli.quinio17@gmail.com>
2025-10-02 16:28:27 +0800

    Update ListTransactions.php

--- app/Filament/Resources/TransactionResource/Pages/ListTransactions.php
+++ app/Filament/Resources/TransactionResource/Pages/ListTransactions.php
@@ -91,9 +91,9 @@ class ListTransactions extends ListRecords
                                 'Other VAT Adjustments' => $record->other_vat_adjustments,
                                 'Total VAT Adjustments' => $record->total_vat_adjusts,
                                 'VAT Payable' => $record->vat - $record->total_vat_adjusts,
-                                'NET Sales' => $record->vatable_sales + $record->total_discounts - $record->total_discounts,
+                                'NET Sales' => $record->vatable_sales + $record->vat_exempt_sales - $record->total_discounts,
                                 'Overflow' => $record->short_over,
-                                'Total Income' => $record->vatable_sales + $record->total_discounts,
+                                'Total Income' => $record->vatable_sales + $record->vat_exempt_sales,
                                 'Reset Counter' => $record->reset_counter,
                                 'Z Counter' => $record->counter,
                             ];
