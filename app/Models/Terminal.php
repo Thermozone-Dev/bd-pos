c536ca9ff42d3c899dbaeca166be92b33c207141
Eli <eli.quinio17@gmail.com>
2025-11-06 02:08:27 +0800

    Add Terminal model and migration

---
+++ app/Models/Terminal.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Terminal extends Model
+{
+    protected $table = 'terminals';
+
+    protected $fillable = [
+        'name',
+    ];
+}
