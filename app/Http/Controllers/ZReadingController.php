4d2aa312057699e2c3ba6d5828ac4b32c275c667
Eli <eli.quinio17@gmail.com>
2025-11-12 00:31:19 +0800

    Update ZReadingController.php

--- app/Http/Controllers/ZReadingController.php
+++ app/Http/Controllers/ZReadingController.php
@@ -216,7 +216,12 @@ class ZReadingController extends Controller
 
         // TRANSACTION SUMMARY
 
-        $totalChange = $transactions->where('transaction_method_id', 1)->where('is_valid', true)->sum('change');
+        $totalChange = $transaction_query->where('is_valid', 1)->
+        with(['paymentMethods' => function ($query) {
+            $query->where('payment_method_id', 1);
+        }])
+        ->get()
+        ->sum('change');
 
         $totalCashPayment = $transaction_query->where('is_valid', true)
         ->withSum(['paymentMethods' => function($query) {
