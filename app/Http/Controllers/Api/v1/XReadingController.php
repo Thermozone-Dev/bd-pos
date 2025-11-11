1ee1732cc070859fd4749d3720077957f20a31c0
Eli <eli.quinio17@gmail.com>
2025-11-12 00:21:50 +0800

    Update XReadingController.php

--- app/Http/Controllers/Api/v1/XReadingController.php
+++ app/Http/Controllers/Api/v1/XReadingController.php
@@ -53,11 +53,14 @@ class XReadingController extends Controller
         $endingFund = $shift->ending_balance;
 
         // $totalChange = $transactions->where('transaction_method_id', 1)->where('is_valid', true)->sum('change');
-        $totalChange = $transactions->with(['paymentMethods' => function ($query) {
-            $query->where('id', 1);
-        }])->where('is_valid', 1)->sum(function ($transaction) {
-            return $transaction->paymentMethods->change() ? $transaction->change : 0;
-        });
+        $totalChange = $transactions->where('is_valid', 1)->
+            withSum(['paymentMethods' => function ($query) {
+                $query->where('payment_method_id', 1);
+            }])->sum(function ($transaction) {
+                return $transaction->paymentMethods->change() ? $transaction->change : 0;
+            });
+
+        dd($totalChange);
 
         // $totalCashPayment = $transactions->where('transaction_method_id', 1)->where('is_valid', true)->sum('total_sales');
         $totalCashPayment = $transactions_query->where('is_valid', true)
