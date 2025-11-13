1935c97992f9c3251fb6b2304840070a0e1296fa
Eli <eli.quinio17@gmail.com>
2025-11-13 19:21:10 +0800

    Update TransactionController.php

--- app/Http/Controllers/Api/v1/TransactionController.php
+++ app/Http/Controllers/Api/v1/TransactionController.php
@@ -963,8 +963,8 @@ class TransactionController extends Controller
                             '    ' . $_item_data->name .
                             '    -' . number_format($_item_data->price, 2) .
                             '    -' . number_format($_item_data->total_value, 2));
-                        $_discount_value += $_item_data['discount_value'];
-                        $_item_count += $_item_data['quantity'];
+                        $_discount_value += $_basket_item['discount_value'];
+                        $_item_count += $_basket_item['quantity'];
                     }
 
                     $_journal_discount_details = [''];
